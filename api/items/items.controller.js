6c4f7386a65f7fb80c35b4bc7dc31553257db6bd
adina9 <adinazwe@gmail.com>
2021-06-29 16:51:31 +0300

    more changes-2

--- api/items/items.controller.js
+++ api/items/items.controller.js
@@ -4,7 +4,6 @@ const logger = require('../../services/logger.service')
 async function getItems(req, res) {
     try {
         const items = await itemService.query()
-        // console.log('items', items);
         res.send(items)
     } catch (err) {
         logger.error('Failed to get items', err)
@@ -13,7 +12,6 @@ async function getItems(req, res) {
 }
 
 async function updateItems(req, res) {
-    console.log('&^&^&', req.body);
     try {
         const items = req.body
         const savedItems = await itemService.update(items)
@@ -27,32 +25,4 @@ async function updateItems(req, res) {
 module.exports = {
     getItems,
     updateItems
-    // getUser,
-    // getUsers,
-    // deleteUser,
-    // updateUser
 }
-
-// async function getUsers(req, res) {
-//     try {
-//         const filterBy = {
-//             txt: req.query?.txt || '',
-//             minBalance: +req.query?.minBalance || 0
-//         }
-//         const users = await itemService.query(filterBy)
-//         res.send(users)
-//     } catch (err) {
-//         logger.error('Failed to get users', err)
-//         res.status(500).send({ err: 'Failed to get users' })
-//     }
-// }
-
-// async function deleteUser(req, res) {
-//     try {
-//         await itemService.remove(req.params.id)
-//         res.send({ msg: 'Deleted successfully' })
-//     } catch (err) {
-//         logger.error('Failed to delete user', err)
-//         res.status(500).send({ err: 'Failed to delete user' })
-//     }
-// }
